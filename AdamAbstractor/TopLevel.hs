6f9a76fc838c49837f442665da9a39bc174f6705
Adam Walker <adam.walker@nicta.com.au>
2013-09-17 17:04:24 -0400

    return consistency constraints

--- AdamAbstractor/TopLevel.hs
+++ AdamAbstractor/TopLevel.hs
@@ -44,7 +44,7 @@ import qualified EqSMTSimple
 compileBin :: STDdManager s u -> VarOps pdb TheVarType s u -> BinExpr ValType -> StateT pdb (ST s) (DDNode s u)
 compileBin m ops = compile m ops . binExpToTSL
 
-newtype R s u = R {unR :: forall pdb. [(VarType EqPred, [DDNode s u])] -> VarOps pdb TheVarType s u -> StateT pdb (ST s) ([DDNode s u])}
+newtype R s u = R {unR :: forall pdb. [(VarType EqPred, [DDNode s u])] -> VarOps pdb TheVarType s u -> StateT pdb (ST s) ([DDNode s u], DDNode s u)}
 
 {-# NOINLINE traceST #-}
 traceST :: String -> ST s ()
@@ -55,7 +55,9 @@ compileUpdate ce m = func <$> abstract ce <*> abstract ce
     where
     func ret dbg = R func2
         where 
-        func2 preds ops = mapM (uncurry pred) preds 
+        func2 preds ops = do
+            res <- mapM (uncurry pred) preds 
+            return (res, bzero m)
             where
             pred (Pred (Predicate.EqVar v1 s1 v2 s2)) x = do
                 --lift $ traceST $ show $ prettyPrint $ abs2Tsl (abs2Ret dbg v1 s1 v2 s2) (text $ pack $ "next")
