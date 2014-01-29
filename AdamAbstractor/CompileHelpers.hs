76e1d6f9fa6f457879a583b87fb2679eea87405b
Adam Walker <adam.walker@nicta.com.au>
2014-01-28 19:10:07 -0500

    move common compilation stuff out of TopLevel

---
+++ AdamAbstractor/CompileHelpers.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE RecordWildCards, PolymorphicComponents, ScopedTypeVariables #-}
+module AdamAbstractor.CompileHelpers where
+
+import Control.Monad.ST
+import Control.Monad.State
+import Control.Applicative
+
+import Text.PrettyPrint.Leijen.Text (text)
+
+import CuddExplicitDeref
+
+import AdamAbstractor.Analysis
+import AdamAbstractor.AST hiding (Pred)
+import AdamAbstractor.Backend as Backend
+import AdamAbstractor.Predicate as Predicate
+import Interface
+
+{-# NOINLINE traceST #-}
+traceST :: String -> ST s ()
+traceST = unsafeIOToST . putStrLn
+
+compileBin :: STDdManager s u -> VarOps pdb TheVarType' s u -> BinExpr ValType -> StateT pdb (ST s) (DDNode s u)
+compileBin m ops = compile m ops . binExpToTSL
+
+newtype R s u = R {unR :: forall pdb. [(VarType EqPred, [DDNode s u])] -> VarOps pdb TheVarType' s u -> StateT pdb (ST s) ([DDNode s u], DDNode s u, DDNode s u)}
+
+compileUpdate :: CtrlExpr String ValType -> STDdManager s u -> Either String (R s u)
+compileUpdate ce m = func <$> abstract ce <*> abstract ce
+    where
+    func ret dbg = R func2
+        where 
+        func2 preds ops = do
+            res <- mapM (uncurry pred) preds 
+            return (res, bzero m, bone m)
+            where
+            pred (Pred (Predicate.EqVar v1 s1 v2 s2)) x = do
+                --lift $ traceST $ show $ prettyPrint $ abs2Tsl (abs2Ret dbg v1 s1 v2 s2) (text $ pack $ "next")
+                compile m ops $ abs2Tsl (abs2Ret ret v1 s1 v2 s2) x
+            pred (Pred (Predicate.EqConst v s c))     x = do
+                --lift $ traceST $ show $ prettyPrint $ equalityConst (abs1Ret dbg v) s c (text $ pack $ "next")
+                compile m ops $ equalityConst (abs1Ret ret v) s c x
+            pred (Enum var)                           x = do
+                --lift $ traceST $ show $ prettyPrint $ passTSL (either (error "func") id (passRet dbg var)) (text $ pack $ "next")
+                compile m ops $ passTSL (either (error "func") id (passRet ret var)) x
+
--- AdamAbstractor/TopLevel.hs
+++ AdamAbstractor/TopLevel.hs
@@ -11,7 +11,6 @@ import Text.Parsec hiding ((<|>))
 import qualified Text.Parsec.Token as T
 import Text.Parsec.Language
 import Control.Error
-import Text.PrettyPrint.Leijen.Text (text)
 import Safe
 import Control.Arrow
 import Data.List
@@ -25,39 +24,14 @@ import AdamAbstractor.Backend as Backend
 import AdamAbstractor.Parser
 import AdamAbstractor.Predicate as Predicate
 import AdamAbstractor.Resolve
+import AdamAbstractor.CompileHelpers
+
 import qualified RefineCommon
 import qualified TermiteGame as Game
 import Interface
 
 import qualified EqSMTSimple
 
-compileBin :: STDdManager s u -> VarOps pdb TheVarType' s u -> BinExpr ValType -> StateT pdb (ST s) (DDNode s u)
-compileBin m ops = compile m ops . binExpToTSL
-
-newtype R s u = R {unR :: forall pdb. [(VarType EqPred, [DDNode s u])] -> VarOps pdb TheVarType' s u -> StateT pdb (ST s) ([DDNode s u], DDNode s u, DDNode s u)}
-
-{-# NOINLINE traceST #-}
-traceST :: String -> ST s ()
-traceST = unsafeIOToST . putStrLn
-
-compileUpdate :: CtrlExpr String ValType -> STDdManager s u -> Either String (R s u)
-compileUpdate ce m = func <$> abstract ce <*> abstract ce
-    where
-    func ret dbg = R func2
-        where 
-        func2 preds ops = do
-            res <- mapM (uncurry pred) preds 
-            return (res, bzero m, bone m)
-            where
-            pred (Pred (Predicate.EqVar v1 s1 v2 s2)) x = do
-                --lift $ traceST $ show $ prettyPrint $ abs2Tsl (abs2Ret dbg v1 s1 v2 s2) (text $ pack $ "next")
-                compile m ops $ abs2Tsl (abs2Ret ret v1 s1 v2 s2) x
-            pred (Pred (Predicate.EqConst v s c))     x = do
-                --lift $ traceST $ show $ prettyPrint $ equalityConst (abs1Ret dbg v) s c (text $ pack $ "next")
-                compile m ops $ equalityConst (abs1Ret ret v) s c x
-            pred (Enum var)                           x = do
-                --lift $ traceST $ show $ prettyPrint $ passTSL (either (error "func") id (passRet dbg var)) (text $ pack $ "next")
-                compile m ops $ passTSL (either (error "func") id (passRet ret var)) x
 
 stdDef = emptyDef {T.reservedNames = reservedNames 
                   ,T.reservedOpNames = reservedOps
